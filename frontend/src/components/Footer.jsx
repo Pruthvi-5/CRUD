c876a67e8bda8b5d7f704293f3d9a3d84fe7744f
Pruthvi-5 <pruthvidm831@gmail.com>
2025-05-29 23:56:50 +0530

    Add files via upload

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div classNmae="d-flex justify-content-center align-items-center p-2 text-white bg-dark" style={{borderTop:"1px solid white"}}>
+        <h5>Developed by the Code Master</h5>
+    </div>
+  );
+};
+
+export default Foote
