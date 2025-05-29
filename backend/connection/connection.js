835e078f652252553e605bd6f78cdc5c64cbd496
Pruthvi-5 <pruthvidm831@gmail.com>
2025-05-29 23:42:46 +0530

    Create connection.js

---
+++ backend/connection/connection.js
@@ -0,0 +1,3 @@
+const mongoose=require("mongoose");
+mongoose.connect("mongodb+srv://pruthvim22beis:tEyPHb332Zd0dHQv@cluster0.whnxvuq.mongodb.net/CRUDop?retryWrites=true&w=majority&appName=Cluster0")
+.then((res)=>console.log("connected"));
