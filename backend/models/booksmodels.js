1fdac9bffa1ecd2e461a093a865df11d4d5db816
Pruthvi-5 <pruthvidm831@gmail.com>
2025-05-29 23:43:34 +0530

    Create booksmodels.js

---
+++ backend/models/booksmodels.js
@@ -0,0 +1,11 @@
+const mongoose=require("mongoose");
+const bookSchema=new mongoose.Schema({
+    bookname:{type:String, requires:true},
+    description:{type:String, requires:true},
+    author:{type:String, requires:true},
+    
+    image:{type:String, requires:true},
+    price:{type:String, requires:true},
+});
+
+module.exports=new mongoose.model("books",bookSchema);
