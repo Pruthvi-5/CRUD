4c659e51bceb9bc2586bdabdc01003e154eef34b
Pruthvi-5 <pruthvidm831@gmail.com>
2025-05-29 23:46:20 +0530

    Create auth.js

---
+++ backend/route/auth.js
@@ -0,0 +1,30 @@
+// backend/routes/auth.js
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+
+router.post("/signup", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(400).json({ message: "Email already exists." });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({ success: true, message: "User registered successfully!" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error." });
+  }
+});
+
+module.exports = router;
